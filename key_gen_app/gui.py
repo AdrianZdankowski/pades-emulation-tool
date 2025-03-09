508f02d631a03832b4e0c0470167a3954af97d34
Adrian Zdankowski <adrianzdankowski@gmail.com>
2025-03-09 18:39:05 +0100

    Improve and finish key generation

--- key_gen_app/gui.py
+++ key_gen_app/gui.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
 import psutil
+import threading
+from CTkMessagebox import CTkMessagebox
 from keygen import GenerateAndSaveKeys
 
 ctk.set_appearance_mode("system")  
@@ -9,23 +11,36 @@ class Gui(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Key generation")
-        self.geometry("400x300")
+        self.geometry("400x400")
 
         vcmd = (self.register(self.validatePin),"%P")
         
+        self.publicKeyPath = None
+        self.DELAY_IN_MS = 1000
+
         self.pendriveListLabel = ctk.CTkLabel(self, text="Available pendrives:")
         self.pendriveComboBox = ctk.CTkComboBox(self, values=[])
         self.detectButton = ctk.CTkButton(self, text="Detect", command=self.detectPendrive)
         self.inputLabel = ctk.CTkLabel(self, text="Input 8-digit PIN code for the encryption key")
         self.inputField = ctk.CTkEntry(self, show="*",validate="key" ,validatecommand=vcmd)
+        self.inputFieldStatus = ctk.CTkLabel(self, text="")
+        self.publicKeyPathLabel = ctk.CTkLabel(self, text="Select where you want to save the private key:")
+        self.publicKeyPathButton = ctk.CTkButton(self, text="Select file path", command=self.selectFileLocation)
+        self.publicKeyPathInfo = ctk.CTkLabel(self, text="")
         self.submitButton = ctk.CTkButton(self, text="Generate", command=self.getPin)
+        self.generationStatusLabel = ctk.CTkLabel(self, text="")
         
         self.pendriveListLabel.pack(padx=20,pady=5)
         self.pendriveComboBox.pack(fill="x",padx=100,pady=5)
         self.detectButton.pack(padx=20,pady=5)
         self.inputLabel.pack(padx=20,pady=5)
         self.inputField.pack(padx=20,pady=5)
-        self.submitButton.pack(padx=20,pady=20)
+        self.inputFieldStatus.pack(padx=20,pady=0)
+        self.publicKeyPathLabel.pack(padx=20,pady=5)
+        self.publicKeyPathButton.pack(padx=20,pady=5)
+        self.publicKeyPathInfo.pack(padx=20,pady=0)
+        self.submitButton.pack(padx=20,pady=10)
+        self.generationStatusLabel.pack(padx=20,pady=0)
 
         self.pendriveComboBox.configure(state="readonly")
 
@@ -42,9 +57,29 @@ class Gui(ctk.CTk):
     def getPin(self):
         pinCode = self.inputField.get()
         selectedPendrive = self.pendriveComboBox.get()
-        print(f"Entered pin: {pinCode}")
-        print(f"Selected pendrive: {selectedPendrive}")
-        GenerateAndSaveKeys(pinCode, selectedPendrive)
+
+        def generateInThread():
+            result = GenerateAndSaveKeys(self, pinCode, selectedPendrive, self.publicKeyPath, self.DELAY_IN_MS)
+            self.after(self.DELAY_IN_MS*6, showMessageBox, result)
+
+        def showMessageBox(result):
+            if result == True:
+                CTkMessagebox(
+                    title="Key generation successful",
+                    message="Private and public keys have been generated and saved! You can close the application now.",
+                    option_1="OK",
+                    icon="check"
+                    )
+            else:
+                CTkMessagebox(
+                    title="Key generation failed",
+                    message="There was an error in saving the keys.",
+                    option_1="Cancel",
+                    icon="cancel"
+                    )
+                
+        thread = threading.Thread(target=generateInThread)
+        thread.start()
 
     def detectPendrive(self):
         self.pendrives = self.searchForPendrive()
@@ -61,19 +96,35 @@ class Gui(ctk.CTk):
             self.submitButton.configure(state="disabled")
             self.pendriveComboBox.configure(values=["No pendrives detected!"])
             self.pendriveComboBox.set("No pendrives detected!")
+
+        if not self.publicKeyPath:
+            self.submitButton.configure(state="disabled")
+
         
     def validatePin(self, value):
         print("Wykonuje walidacje")
         if value.isdigit() and len(value) <= 8:
             if len(value) == 8:
                 self.submitButton.configure(state="normal")
+                self.inputFieldStatus.configure(text="PIN is valid.", text_color="green")
             else:
                 self.submitButton.configure(state="disabled")
+                self.inputFieldStatus.configure(text="PIN is invalid!", text_color="red")
             return True
         else:
             self.submitButton.configure(state="disabled")
+            self.inputFieldStatus.configure(text="PIN is invalid!", text_color="red")
             return False
     
+    def selectFileLocation(self):
+        path = ctk.filedialog.askdirectory(title="Select public key save location")
+
+        if path:
+            self.publicKeyPath = path
+            self.publicKeyPathInfo.configure(text=f"Selected path:\n{path}")
+
+    def setGenerationStatusLabelText(self, state):
+        self.generationStatusLabel.configure(text=state)
 
 
 
--- key_gen_app/keygen.py
+++ key_gen_app/keygen.py
@@ -6,25 +6,44 @@ from hashlib import sha256
 import os
 
 
-def GenerateAndSaveKeys(pin, pendrive):
-    pseudoRandomGenerator = get_random_bytes
-    hashedPin = sha256(pin.encode()).digest()
-    key = RSA.generate(4096, randfunc=pseudoRandomGenerator)
-    privateKey = key.export_key()
-    publicKey = key.public_key().export_key()
+def GenerateAndSaveKeys(gui, pin, pendrive, publicKeyPath, DELAY_IN_MS):
+    print(pin)
+    print(pendrive)
+    print(publicKeyPath)
+    try:
+        gui.after(0, gui.setGenerationStatusLabelText, "Generating PIN hash...")
+        pseudoRandomGenerator = get_random_bytes
+        hashedPin = sha256(pin.encode()).digest()
 
-    cipher = AES.new(hashedPin, AES.MODE_CBC)
-    iv = cipher.iv
-    encryptedPrivateKey = iv + cipher.encrypt(pad(privateKey, AES.block_size))
+        gui.after(DELAY_IN_MS, gui.setGenerationStatusLabelText, "Generating private and public key...")
+        key = RSA.generate(4096, randfunc=pseudoRandomGenerator)
+        privateKey = key.export_key()
+        publicKey = key.public_key().export_key()
 
-    keyFolder = os.path.join(pendrive,"Key")
-    os.makedirs(keyFolder,exist_ok=True)
+        gui.after(DELAY_IN_MS*2, gui.setGenerationStatusLabelText, "Encrypting private key...")
+        cipher = AES.new(hashedPin, AES.MODE_CBC)
+        iv = cipher.iv
+        encryptedPrivateKey = iv + cipher.encrypt(pad(privateKey, AES.block_size))
 
-    privateKeyPath = os.path.join(keyFolder, "encrypted_PK.bin")
-    
-    with open(privateKeyPath, "wb") as f:
-        f.write(encryptedPrivateKey)
+        keyFolder = os.path.join(pendrive,"Key")
+        os.makedirs(keyFolder,exist_ok=True)
 
+        privateKeyPath = os.path.join(keyFolder, "encrypted_PK.bin")
+        publicKeyPath = os.path.join(publicKeyPath, "public_key.pem")
+        
+        gui.after(DELAY_IN_MS*3, gui.setGenerationStatusLabelText, "Saving private key...")
+        with open(privateKeyPath, "wb") as f:
+            f.write(encryptedPrivateKey)
+
+        gui.after(DELAY_IN_MS*4, gui.setGenerationStatusLabelText, "Saving public key...")
+        with open(publicKeyPath, "wb") as f:
+            f.write(publicKey)
+
+        gui.after(DELAY_IN_MS*5, gui.setGenerationStatusLabelText, "Generation and saving succeeded!")
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
    
 
 
