f8d4d3c84f5b8629afbdef8682bf2bb455300281
arekflis <arkadiuszflisikowski10@gmail.com>
2025-03-29 20:33:50 +0100

    Add PDF signing and verification features

--- main_application/gui.py
+++ main_application/gui.py
@@ -1,11 +1,22 @@
-import customtkinter as ctk
+import customtkinter as ctk, time
 from utils.commonFunctions import searchForPendrive, searchAndReadPrivateKey
-from main_application.signAndVerifyPDF import verifyPin
+from main_application.signAndVerifyPDF import signPdfAndVerifyPin, verifyPdfFiles
+from CTkMessagebox import CTkMessagebox
 
 MENU_STATE_MAIN = 1
 MENU_STATE_SIGNING = 2
 MENU_STATE_VERIFYING = 3
 PRIVATE_KEY_PATH = "Key/encrypted_PK.bin"
+
+NO_SIGNATURE = 1
+SIGNATURE_VERIFICATION_VALUE_ERROR = 2
+UNKNOWN_ERROR = 3
+
+INFO_ABOUT_SIGNING_DOCUMENT = 1
+INFO_ABOUT_VERIFYING_DOCUMENT = 2
+
+PDF_FILE = 1
+PUBLIC_KEY_FILE = 2
 
 ctk.set_appearance_mode("system")  
 ctk.set_default_color_theme("blue")  
@@ -28,31 +39,33 @@ class Gui(ctk.CTk):
         self.inputField = ctk.CTkEntry(self, show="*",validate="key" ,validatecommand=self.vcmd, width=280, height=30)
         self.inputFieldStatus = ctk.CTkLabel(self, text="")
 
-        self.pdfFilePathButton = ctk.CTkButton(self, width=280, height=60, text="Select file path", font=("Arial", 20), command=self.selectFileLocation)
+        self.pdfFilePathButton = ctk.CTkButton(self, width=280, height=60, text="Select file path", font=("Arial", 20), command=lambda:self.selectFileLocation("Select the PDF file", [("PDF files", "*.pdf")], PDF_FILE))
         self.pdfFilePathInfo = ctk.CTkLabel(self, text="")
+
+        self.publicKeyPathButton = ctk.CTkButton(self, width=280, height=60, text="Select public key path", font=("Arial", 20), command=lambda:self.selectFileLocation("Select the Public Key file", [("PEM files", "*.pem")], PUBLIC_KEY_FILE))
+        self.publicKeyPathInfo = ctk.CTkLabel(self, text="")
         
         self.submitSignButton = ctk.CTkButton(self, width=280, height=60, text="Submit", font=("Arial", 20), command=self.signPdfFile)
         self.infoAboutSigningDocument = ctk.CTkLabel(self, text="", font=("Arial", 20))
 
         self.submitVerifyButton = ctk.CTkButton(self, width=280, height=60, text="Submit", font=("Arial", 20), command=self.verifyPdfFile)
-        self.infoAboutVerifyingDocument = ctk.CTkLabel(self, text="The PDF file is identical to the originally signed document!", font=("Arial", 20), text_color="green")
+        self.infoAboutVerifyingDocument = ctk.CTkLabel(self, text="", font=("Arial", 20))
         
-        self.returnToMenuButton = ctk.CTkButton(self, width=280, height=60, text="Back", font=("Arial", 20), command=self.returnToMenu)
+        self.returnToMenuButton = ctk.CTkButton(self, width=180, height=40, text="Back", font=("Arial", 15), command=self.returnToMenu)
 
         self.pendrives = []
         self.privateKey = ""
-        self.DELAY_IN_MS = 1000
 
         self.titleLabel.pack(pady=(40, 20))
         self.checkIfPendriveSearched()
 
         self.isPdfFileSelected = False
+        self.isPublicKeySelected = False
         self.isValidPin = False
         self.menuState = MENU_STATE_MAIN
 
-
     def checkIfPendriveSearched(self):
-        if self.detectPendrive():
+        if self.detectPendriveAndReadPrivateKey():
             if self.infoAboutPendrive.winfo_ismapped():
                 self.infoAboutPendrive.pack_forget()
             if self.detectPendriveButton.winfo_ismapped():
@@ -69,7 +82,7 @@ class Gui(ctk.CTk):
             self.infoAboutPendrive.pack(pady=(80,20))
             self.detectPendriveButton.pack(pady=(50,0))
                         
-    def detectPendrive(self):
+    def detectPendriveAndReadPrivateKey(self):
         self.pendrives = searchForPendrive()
         if len(self.pendrives) == 0:
             return False
@@ -104,11 +117,13 @@ class Gui(ctk.CTk):
         self.menuState = MENU_STATE_VERIFYING
         self.resetMainMenu()
 
-        self.pdfFilePathButton.pack(pady=(60,0))
-        self.pdfFilePathInfo.pack(pady=(30,0))
-        self.submitVerifyButton.pack(pady=(30,0))
-        self.infoAboutVerifyingDocument.pack(pady=(30,0))
-        self.returnToMenuButton.pack(pady=(45,0))
+        self.pdfFilePathButton.pack(pady=(15,0))
+        self.pdfFilePathInfo.pack(pady=(15,0))
+        self.publicKeyPathButton.pack(pady=(15,0))
+        self.publicKeyPathInfo.pack(pady=(15,0))
+        self.submitVerifyButton.pack(pady=(15,0))
+        self.infoAboutVerifyingDocument.pack(pady=(15,0))
+        self.returnToMenuButton.pack(pady=(25,0))
 
         self.submitVerifyButton.configure(state="disabled")
 
@@ -129,15 +144,22 @@ class Gui(ctk.CTk):
             self.updateMenuState()
             return False
         
-    def selectFileLocation(self):
-        path = ctk.filedialog.askopenfilename(title="Select the PDF file", filetypes=[("PDF files", "*.pdf")])
+    def selectFileLocation(self, title, filetypes, type):
+        path = ctk.filedialog.askopenfilename(title=title, filetypes=filetypes)
 
         if path:
-            self.isPdfFileSelected = True
-            self.pdfFilePathInfo.configure(text=f"Selected path:\n{path}", font=("Arial", 20))
+            if type == PDF_FILE:
+                self.isPdfFileSelected = True
+                self.pdfFilePathInfo.configure(text=f"Selected path:\n{path}", font=("Arial", 20))
+            elif type == PUBLIC_KEY_FILE:
+                self.isPublicKeySelected = True
+                self.publicKeyPathInfo.configure(text=f"Selected path:\n{path}", font=("Arial", 20))
         else:
-            self.isPdfFileSelected = False
-        
+            if type == PDF_FILE:
+                self.isPdfFileSelected = False
+            elif type == PUBLIC_KEY_FILE:
+                self.isPublicKeySelected = False
+
         self.updateMenuState()
 
     def updateMenuState(self):
@@ -153,7 +175,7 @@ class Gui(ctk.CTk):
             self.submitSignButton.configure(state="disabled")
 
     def updateVerifyPdfFileMenuState(self):
-        if self.isPdfFileSelected:
+        if self.isPdfFileSelected and self.isPublicKeySelected:
             self.submitVerifyButton.configure(state="normal")
         else:
             self.submitVerifyButton.configure(state="disabled")
@@ -175,8 +197,13 @@ class Gui(ctk.CTk):
         self.returnToMenuButton.pack_forget()
 
     def resetVerifyPdfFileMenu(self):
+        self.publicKeyPathInfo.configure(text="")
+        self.infoAboutVerifyingDocument.configure(text="")
+
         self.pdfFilePathButton.pack_forget()
         self.pdfFilePathInfo.pack_forget()
+        self.publicKeyPathButton.pack_forget()
+        self.publicKeyPathInfo.pack_forget()
         self.submitVerifyButton.pack_forget()
         self.infoAboutVerifyingDocument.pack_forget()
         self.returnToMenuButton.pack_forget()
@@ -184,28 +211,60 @@ class Gui(ctk.CTk):
     def returnToMenu(self):
         self.isValidPin = False
         self.isPdfFileSelected = False
+        self.isPublicKeySelected = False
         self.pdfFilePathInfo.configure(text="")
 
         if self.menuState == MENU_STATE_SIGNING:
-            self.menuState = MENU_STATE_MAIN
             self.resetSignPdfFileMenu()     
         elif self.menuState == MENU_STATE_VERIFYING:
-            self.menuState = MENU_STATE_MAIN
             self.resetVerifyPdfFileMenu()
-            
+        
+        self.menuState = MENU_STATE_MAIN
         self.signFileMenuButton.pack(pady=(100,20))
         self.verifyFileMenuButton.pack(pady=(70.0))
+    
+    def setLabelText(self, newText, labelType, color=None):
+        if labelType == INFO_ABOUT_SIGNING_DOCUMENT:
+            self.infoAboutSigningDocument.configure(text=newText)
+            if color:
+                self.infoAboutSigningDocument.configure(text_color=color)
+        elif labelType == INFO_ABOUT_VERIFYING_DOCUMENT:
+            self.infoAboutVerifyingDocument.configure(text=newText)
+            if color:
+                self.infoAboutVerifyingDocument.configure(text_color=color)
+        if newText != "":
+            time.sleep(1)
 
-    def setSigningPDFStatusLabelText(self, newState, color):
-        self.infoAboutSigningDocument.configure(text=newState, text_color=color)
+    def showMessageBox(self, title, message, option, icon):
+        CTkMessagebox(
+            title=title,
+            message=message,
+            option_1=option,
+            icon=icon
+        )
+
+    def signPdfMessageBox(self, result):
+        if result:
+            self.showMessageBox("PDF Signing Completed Successfully", "The signed PDF file has been saved.", "OK", "check")
+        else:
+            self.showMessageBox("PDF Signing Failed", "An error occurred while signing the PDF file.", "Cancel", "cancel")
+
+    def incorrectPinMessageBox(self):
+        self.showMessageBox("Incorrect PIN Entered", "The PIN you entered is incorrect. Please try again.", "Cancel", "cancel")
+    
+    def verifyPdfSuccessfullyMessageBox(self):
+        self.showMessageBox("PDF Signature Verification", "The PDF signature has been successfully verified.", "OK", "check")
+
+    def verifyPdfErrorMessageBox(self, errorType):
+        if errorType == NO_SIGNATURE:
+            self.showMessageBox("PDF Signature Verification", "No signature found in the PDF document.", "Cancel", "cancel")
+        elif errorType == SIGNATURE_VERIFICATION_VALUE_ERROR:
+            self.showMessageBox("PDF Signature Verification", "The signature could not be verified due to an invalid value in the signature data.", "Cancel", "cancel")
+        else:
+            self.showMessageBox("PDF Signature Verification", "An unknown error occurred during PDF signature verification.", "Cancel", "cancel")
 
     def signPdfFile(self):
-        self.after(0, self.setSigningPDFStatusLabelText, "Signing your PDF file ...", "white")
-        self.after(self.DELAY_IN_MS, self.setSigningPDFStatusLabelText, "Verifying your PIN ...", "white")
-        if verifyPin(self.inputField.get(), self.privateKey):
-            self.after(self.DELAY_IN_MS * 2, self.setSigningPDFStatusLabelText, "Your PIN is correct", "green")
-        else:
-            self.after(self.DELAY_IN_MS * 2, self.setSigningPDFStatusLabelText, "Your PIN is incorrect", "red")
+        signPdfAndVerifyPin(self, self.privateKey, self.inputField.get(), self.pdfFilePathInfo.cget("text").split("\n")[1])
 
     def verifyPdfFile(self):
-        pass
+        verifyPdfFiles(self, self.pdfFilePathInfo.cget("text").split("\n")[1], self.publicKeyPathInfo.cget("text").split("\n")[1])
--- main_application/signAndVerifyPDF.py
+++ main_application/signAndVerifyPDF.py
@@ -1,17 +1,32 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from hashlib import sha256
+from Crypto.Signature import pkcs1_15
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from PyPDF2 import PdfReader, PdfWriter
+import os, threading
+
+NO_SIGNATURE = 1
+SIGNATURE_VERIFICATION_VALUE_ERROR = 2
+UNKNOWN_ERROR = 3
+OK = 4
+
+INFO_ABOUT_SIGNING_DOCUMENT = 1
+INFO_ABOUT_VERIFYING_DOCUMENT = 2
+
+def decryptPrivateKey(pin, privateKey):
+    iv = privateKey[:16]
+    cipherText = privateKey[16:]
+
+    hashedPin = sha256(pin.encode()).digest()
+
+    cipher = AES.new(hashedPin, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(cipherText), AES.block_size)
 
 def verifyPin(pin, privateKey):
     try:
-        iv = privateKey[:16] 
-        cipherText = privateKey[16:]
-
-        hashedPin = sha256(pin.encode()).digest()
-
-        cipher = AES.new(hashedPin, AES.MODE_CBC, iv)
-        
-        decryptedPrivateKey = unpad(cipher.decrypt(cipherText), AES.block_size)
+        decryptedPrivateKey = decryptPrivateKey(pin, privateKey)
 
         if decryptedPrivateKey:
            return True
@@ -20,3 +35,146 @@ def verifyPin(pin, privateKey):
     except Exception as e:
         return False
 
+def createHash(pdfReader):
+    hash = SHA256.new()
+    for page in pdfReader.pages:
+        hash.update(page.extract_text().encode())
+    return hash
+
+def signPDF(gui, privateKey, pin, pdfPath):
+    try:
+        base, ext = os.path.splitext(pdfPath)
+        signedPdfPath = f"{base}-signed.pdf"
+        decryptedPrivateKey = RSA.import_key(decryptPrivateKey(pin, privateKey))
+
+        gui.setLabelText("Opening the PDF file ...", INFO_ABOUT_SIGNING_DOCUMENT, "white")
+        with open(pdfPath, "rb") as f:
+            pdfReader = PdfReader(f)
+            pdfWriter = PdfWriter()
+
+            gui.setLabelText("Generating hash of the PDF file ...", INFO_ABOUT_SIGNING_DOCUMENT)
+            pdfHash = createHash(pdfReader)
+
+            gui.setLabelText("Creating a digital signature ...", INFO_ABOUT_SIGNING_DOCUMENT)
+            signature = pkcs1_15.new(decryptedPrivateKey).sign(pdfHash)
+
+            gui.setLabelText("Adding a signature to the PDF file ...", INFO_ABOUT_SIGNING_DOCUMENT)
+            for page in pdfReader.pages:
+                pdfWriter.add_page(page)
+
+            pdfWriter.add_metadata({
+                "/Signature": signature
+            })
+
+            gui.setLabelText("Saving the signed PDF file ...", INFO_ABOUT_SIGNING_DOCUMENT)
+            with open(signedPdfPath, "wb") as signedPDF:
+                pdfWriter.write(signedPDF)
+
+            return True
+    except Exception as e:
+        print(f"Error signing PDF: {e}")
+        return False
+
+def signPdfAndVerifyPin(gui, privateKey, pin, pdfPath):
+    def signPdfThread():
+        gui.setLabelText("Starting the PDF signing process ...", INFO_ABOUT_SIGNING_DOCUMENT)
+        gui.setLabelText("Verifying PIN ...", INFO_ABOUT_SIGNING_DOCUMENT)
+        
+        if verifyPin(pin, privateKey):
+            gui.setLabelText("PIN verified successfully", INFO_ABOUT_SIGNING_DOCUMENT, "green")
+            if signPDF(gui, privateKey, pin, pdfPath):
+                gui.setLabelText("", INFO_ABOUT_SIGNING_DOCUMENT)
+                gui.signPdfMessageBox(True)
+            else:
+                gui.setLabelText("", INFO_ABOUT_SIGNING_DOCUMENT)
+                gui.signPdfMessageBox(False)
+        else:
+            gui.setLabelText("", INFO_ABOUT_SIGNING_DOCUMENT)
+            gui.incorrectPinMessageBox()
+
+    threading.Thread(target=signPdfThread, daemon=True).start()
+
+def verifyPdfSignatureFromMetadata(gui, pdfPath, publicKeyPath):
+    try:
+        gui.setLabelText("Reading the public key from file ...", INFO_ABOUT_VERIFYING_DOCUMENT)
+        with open(publicKeyPath, "rb") as pub_file:
+            public_key = RSA.import_key(pub_file.read())
+
+        gui.setLabelText("Reading the PDF file ...", INFO_ABOUT_VERIFYING_DOCUMENT)
+        with open(pdfPath, "rb") as f:
+            pdfReader = PdfReader(f)
+            metadata = pdfReader.metadata
+
+            gui.setLabelText("Checking if signature exists in metadata ...", INFO_ABOUT_VERIFYING_DOCUMENT)
+            if "/Signature" not in metadata:
+                return NO_SIGNATURE
+
+            signature = metadata["/Signature"]
+
+            gui.setLabelText("Generating hash of the PDF file ...", INFO_ABOUT_VERIFYING_DOCUMENT)
+            pdfHash = createHash(pdfReader)
+
+            gui.setLabelText("Verifying the signature ...", INFO_ABOUT_VERIFYING_DOCUMENT)
+            try:
+                pkcs1_15.new(public_key).verify(pdfHash, signature)
+                return OK
+            except (ValueError, TypeError) as e:
+                return SIGNATURE_VERIFICATION_VALUE_ERROR
+    except Exception as e:
+        print(f"Error verifying PDF signature: {e}")
+        return UNKNOWN_ERROR
+        
+def verifyPdfFiles(gui, pdfPath, publicKeyPath):
+    def verifyPdfThread():
+        gui.setLabelText("Starting the PDF verification process ...", INFO_ABOUT_VERIFYING_DOCUMENT)
+
+        result = verifyPdfSignatureFromMetadata(gui, pdfPath, publicKeyPath)
+        gui.setLabelText("", INFO_ABOUT_VERIFYING_DOCUMENT)
+        if result == OK:
+            gui.verifyPdfSuccessfullyMessageBox()
+        elif result == NO_SIGNATURE:
+            gui.verifyPdfErrorMessageBox(NO_SIGNATURE)
+        elif result == SIGNATURE_VERIFICATION_VALUE_ERROR:
+            gui.verifyPdfErrorMessageBox(SIGNATURE_VERIFICATION_VALUE_ERROR)
+        else:
+            gui.verifyPdfErrorMessageBox(UNKNOWN_ERROR)
+
+    threading.Thread(target=verifyPdfThread, daemon=True).start()
+
+def checkPdfSignature(pdfPath):
+    try:    
+        with open(pdfPath, "rb") as f:
+            pdfReader = PdfReader(f)
+            metadata = pdfReader.metadata
+
+            if metadata and "/Signature" in metadata:
+                print("Signature found in the PDF metadata:", metadata["/Signature"])
+            else:
+                print("No signature found in the PDF metadata.")
+    except Exception as e:
+        print(f"Error reading the PDF file: {e}")
+
+def modifyPdf(pdfPath):
+    try:
+        reader = PdfReader(pdfPath)
+        writer = PdfWriter()
+
+        signature = reader.metadata["/Signature"]
+
+        for page in range(len(reader.pages)):
+            writer.add_page(reader.pages[page])
+
+        writer.add_page(reader.pages[0])
+
+        writer.add_metadata({
+            "/Signature": signature
+        })
+
+        with open(pdfPath, "wb") as f:
+            writer.write(f)
+    except Exception as e:
+        print(f"Error modifying the PDF file: {e}")
+
+#checkPdfSignature("")
+#checkPdfSignature("")
+#modifyPdf("")
